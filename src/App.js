d2a5226c7ed67696ad28fb7d77085c0b950dc799
unknown <johnloydcorletadobas@gmail.com>
2023-08-18 16:22:48 +0800

    add hide functionality to the sidebar

--- src/App.js
+++ src/App.js
@@ -8,34 +8,41 @@ import Conversation from './components/Conversation';
 
 export default function App() {
 
-  // useState variables
-  const [signIn, setSignIn] = useState(true);
+  // // useState variables
+  // const [signIn, setSignIn] = useState(true);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-  // Handle changes for input tags
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  }
+  // // Handle changes for input tags
+  // const handleNameChange = (event) => {
+  //   setName(event.target.value);
+  // }
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  }
+  // const handleEmailChange = (event) => {
+  //   setEmail(event.target.value);
+  // }
   
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  }
+  // const handlePasswordChange = (event) => {
+  //   setPassword(event.target.value);
+  // }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    window.alert(`Name: ${name}, Email: ${email}, Password: ${password} `);
-  }
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   window.alert(`Name: ${name}, Email: ${email}, Password: ${password} `);
+  // }
 
-  // Keeps track whether the user wants to sign in or create an account.
-  const handleSignInStatus = () => {
-    setSignIn(!signIn);
+  // // Keeps track whether the user wants to sign in or create an account.
+  // const handleSignInStatus = () => {
+  //   setSignIn(!signIn);
+  // }
+
+  const [expand, setExpand] = useState(false);
+
+  const handleExpandButton = () => {
+
+    setExpand(!expand);
   }
 
   return (
@@ -72,8 +79,14 @@ export default function App() {
     //     </form>
     //   </section>
     // </div>
-    <div className="App">
-      <Sidebar />
+    <div className="App"
+        style={{ 
+        display: "grid",
+        gridTemplateColumns: expand ? "245px 350px calc(100vw - 595px)" : "85px 350px calc(100vw - 435px)",
+        gridTemplateRows: "100vh"
+      }}
+    >
+      <Sidebar expand={expand} onClick={handleExpandButton} />
       <Chat />
       <Conversation />
     </div>
